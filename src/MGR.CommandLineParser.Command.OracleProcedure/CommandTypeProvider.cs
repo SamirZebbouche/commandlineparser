f83d357aa9b8abc18cbdeba9d687ccd9ed08a9c7
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2020-08-24 17:57:21 +0200

    Start implementation of the retrival of the procedures

---
+++ src/MGR.CommandLineParser.Command.OracleProcedure/CommandTypeProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MGR.CommandLineParser.Extensibility.Command;
+using Microsoft.EntityFrameworkCore;
+
+namespace MGR.CommandLineParser.Command.OracleProcedure
+{
+    internal class CommandTypeProvider : ICommandTypeProvider
+    {
+        private readonly OracleSystemDataContext _dbContext;
+
+        public CommandTypeProvider(OracleSystemDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<ICommandType> GetAllCommandTypes()
+        {
+            var procedures = _dbContext.Procedures.Include(procedure => procedure.Parameters).ToList();
+            return procedures.Select(MapProcedureToCommandType);
+        }
+        public ICommandType GetCommandType(string commandName)
+        {
+            var procedure = _dbContext.Procedures.Include(procedure => procedure.Parameters).Where(procedure => procedure.Name == commandName).SingleOrDefault();
+            if (procedure != null)
+            {
+                return MapProcedureToCommandType(procedure);
+            }
+            return null;
+        }
+
+        private static CommandType MapProcedureToCommandType(Procedure procedure) => new CommandType(new CommandMetadata(procedure.Name),
+                procedure.Parameters.Where(parameter => parameter.Direction.HasFlag(Direction.In)).Select(parameter => new CommandOptionMetadata(
+                    new OptionDisplayInfo(parameter.Name),
+                    !parameter.HasDefaultValue,
+                    parameter.DefaultValue)),
+                procedure.Parameters.Where(parameter => parameter.Direction.HasFlag(Direction.Out));
+    }
+}
--- src/MGR.CommandLineParser.Command.OracleProcedure/OracleSystemDataContext.cs
+++ src/MGR.CommandLineParser.Command.OracleProcedure/OracleSystemDataContext.cs
@@ -4,6 +4,13 @@ namespace MGR.CommandLineParser.Command.OracleProcedure
 {
     internal sealed class OracleSystemDataContext : DbContext
     {
+        public DbSet<Procedure> Procedures { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProcedureMap())
+                .ApplyConfiguration(new ParameterMap());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
